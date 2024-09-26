6b3b91368c40fb9e6a1aefabf66f147bbdd8b36e
Ntshebetseng Moloi <nana13.moloi@gmail.com>
2024-09-27 00:13:51 +0200

    uploading code

---
+++ index.js
@@ -0,0 +1,56 @@
+try {
+    const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=pastel")
+    const data = await res.json()
+    document.body.style.backgroundImage = `url(${data.urls.full})`
+    document.getElementById("author").textContent = `By: ${data.user.name}`
+} catch(err){
+    document.querySelector("body").style.backgroundImage = `url(https://images.unsplash.com/photo-1529034502960-57f42a966080?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MjczODY1ODN8&ixlib=rb-4.0.3&q=85)`
+    document.getElementById("author").textContent = `by Patrick Ryan`
+    }
+
+
+try {
+    const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    if (!res.ok) {
+        throw Error("Something went wrong")
+    }
+    const data = await res.json()
+    document.getElementById("crypto-top").innerHTML = `
+        <img src=${data.image.small} />
+        <span>${data.name}</span>
+    `
+    document.getElementById("crypto").innerHTML += `
+        <p>🎯: R${data.market_data.current_price.zar}</p>
+        <p>👆: R${data.market_data.high_24h.zar}</p>
+        <p>👇: R${data.market_data.low_24h.zar}</p>
+    `
+} catch (err) {
+    console.error(err)
+}
+
+function getCurrentTime() {
+    const date = new Date()
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", { timeStyle: "short" })
+}
+
+setInterval(getCurrentTime, 1000)
+
+navigator.geolocation.getCurrentPosition(async position => {
+    try {
+        const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        if (!res.ok) {
+            throw Error("Weather data not available")
+        }
+        const data = await res.json()
+        const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        document.getElementById("weather").innerHTML = `
+            <img src=${iconUrl} />
+            <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+            <p class="weather-city">${data.name}</p>
+        `
+    } catch (err) {
+        console.error(err)
+    }
+});
+
+
